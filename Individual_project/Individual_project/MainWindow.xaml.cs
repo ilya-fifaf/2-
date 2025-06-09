7650a48b5cce68d02435b0cdca52440fee271662
Илья Чернов Дмитриевич <111155698+ilya-fifaf@users.noreply.github.com>
2025-06-09 20:43:28 +0300

    Индивидуальный проект - v.1

---
+++ Individual_project/Individual_project/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Individual_project;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+    {
+        MessageBox.Show(login.Text);
+    }
+
+    private void Registration_Click (object sender, RoutedEventArgs e)
+    {
+        Registration registration = new Registration();
+        registration.Show();
+        this.Close();
+    }
+}
---
+++ Individual_project/Individual_project/Registration.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace Individual_project;
+
+public partial class Registration : Window
+{
+    public Registration()
+    {
+        InitializeComponent();
+    }
+
+    private void Login_Click(object sender, RoutedEventArgs e)
+    {
+        MainWindow mainWindow = new MainWindow();
+        mainWindow.Show();
+        this.Close();
+    }
+}
